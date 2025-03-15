5942883110a9293737fac5d252ba34cb0f0be4dd
Ayush Sharma <ayushxyz04@gmail.com>
2025-03-15 23:05:59 +0000

    update text size and env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[2rem]">
             Deploy on <span className="text-[hsl(280,100%,70%)]">AWS using SST</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
